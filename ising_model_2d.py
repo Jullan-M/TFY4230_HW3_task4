6b344e98e7e3f1bd3b02628a8060c50c51b6ac23
jullan-m <jmlindi@stud.ntnu.no>
2020-05-13 17:16:45 +0200

    Finished (finally lol)

--- ising_model_2d.py
+++ ising_model_2d.py
@@ -1,27 +1,32 @@
 __author__ = 'Jullan'
 # -*- coding: utf-8 -*-
-#Made by Jullan
+# Made by Jullan
 import numpy as np
 from sympy.utilities.iterables import permute_signs
 import timeit
 from matplotlib import pyplot as plt
+
 
 def get_cmap(n, name='hsv'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
 
+
 KB = 1.38064852e-23
-J_PAIR = np.array([1,2])
+J_PAIR = np.array([1, 2])
 B = 5
 BETA = 1
 
 #   Generates the possible 2^n spin-configurations
 #   for the n spins omega_m,i on the n chains at a
 #   given i.
+
+
 def generate_spin_conf(n):
     sample_tup = tuple(np.ones(n))
     return np.asarray(list(permute_signs(sample_tup)))
+
 
 def generate_spin_conf_slow(n):
     #   Slow, but intuitive implementation
@@ -30,6 +35,7 @@ def generate_spin_conf_slow(n):
         for k in range(n):
             sigma[l][k] = (-1)**(l//(2**k))
     return sigma
+
 
 def generate_P_matrix(sigma_arr, J, B, k, n, beta):
     #   Old, unused function
@@ -41,16 +47,22 @@ def generate_P_matrix(sigma_arr, J, B, k, n, beta):
             site_sum = np.sum(sigma_arr[l]*sigma_arr[m])
             chain_sum = np.sum(sigma_arr[l]*np.roll(sigma_arr[l], -1))
             b_sum = np.sum(sigma_arr[l] + sigma_arr[m])
-            P_arr[l, m] = np.exp( beta * (J[0] * site_sum + J[1] * chain_sum + B/2 * b_sum))
+            P_arr[l, m] = np.exp(
+                beta * (J[0] * site_sum + J[1] * chain_sum + B/2 * b_sum))
     return P_arr
 
+
 class Ising_2D:
-    def __init__(self, n, beta = BETA, kB = KB, J = J_PAIR, B = B):
+    def __init__(self, n, beta=BETA, kB=KB, J=J_PAIR, B=B):
         #   Various constants/variables
         self.beta = beta
         self.kB = kB
         self.J = J
         self.B = B
+        if (n == 2):
+            self.B = B/2
+        elif (n == 1):
+            self.B = 0
 
         #   Length of arrays, chains, sites.
         self.n = n
@@ -58,9 +70,9 @@ class Ising_2D:
 
         #   Arrays/matrices
         self.sigma_arr = generate_spin_conf(self.n)
-        self.P_matrix_nopow = np.zeros((self.P_len, self.P_len))
-        self.P_matrix = np.zeros((self.P_len, self.P_len))
-        self.lambdas = np.zeros(self.P_len)
+        self.P_matrix_nopow = np.empty((self.P_len, self.P_len))
+        self.P_matrix = np.empty_like(self.P_matrix_nopow)
+        self.generate_P_matrix()
 
     def update_sigma_arr(self):
         self.sigma_arr = generate_spin_conf(self.n)
@@ -69,38 +81,47 @@ class Ising_2D:
         for l in range(self.P_len):
             for m in range(self.P_len):
                 #   These _sum-variables are sums over the chains, k.
-                site_sum = np.sum(self.sigma_arr[l] * self.sigma_arr[m])
-                chain_sum = np.sum(self.sigma_arr[l] * np.roll(self.sigma_arr[l], -1))
+                site_sum = np.dot(self.sigma_arr[l], self.sigma_arr[m])
+                chain_sum = np.dot(
+                    self.sigma_arr[l], np.roll(self.sigma_arr[l], -1))
                 b_sum = np.sum(self.sigma_arr[l] + self.sigma_arr[m])
 
-                self.P_matrix_nopow[l, m] = np.exp(self.J[0] * site_sum + self.J[1] * chain_sum + self.B / 2 * b_sum)
+                self.P_matrix_nopow[l, m] = np.exp(
+                    self.J[0] * site_sum + self.J[1] * chain_sum + self.B / 2 * b_sum)
         self.P_matrix = self.P_matrix_nopow ** self.beta
 
     def refresh_P_matrix_beta(self):
         self.P_matrix = self.P_matrix_nopow**self.beta
 
     def find_lambdas(self):
-        self.lambdas = np.linalg.eigvals(self.P_matrix)
+        return np.linalg.eigvals(self.P_matrix)
 
 
-def lambda_afo_beta(ising, beta_low, beta_high, dbeta):
-    ising.generate_P_matrix()
-    steps = int((beta_high - beta_low) / dbeta)
-    lambdas_arr = np.zeros((steps, 2**ising.n))
-    for i in range(steps):
+def lambda_beta(ising, betas):
+    lambdas_beta = np.empty((len(betas), 2**ising.n))
+    for j, beta in enumerate(betas):
         #print("hey!,", i)
-        ising.beta = beta_low + dbeta * i
+        ising.beta = beta
         ising.refresh_P_matrix_beta()
-        ising.find_lambdas()
-        lambdas_arr[i] = np.real(ising.lambdas)
-    return lambdas_arr
+        lambdas_beta[j] = np.real(ising.find_lambdas())
+    return np.sort(lambdas_beta)
 
-def m_afo_bB(ising, bB_low, bB_high, dbB):
-    ising.generate_P_matrix()
-    steps = int((bB_high - bB_low) / dbB)
+
+def lambda_betaB(ising, Bs, betas):
+    lambdas_betaB = np.empty((len(Bs), len(betas)))
+    for i, B in enumerate(Bs):
+        ising.B = B
+        ising.generate_P_matrix()
+        lambdas_betaB[i] = lambda_beta(ising, betas)[:, -1]
+    return lambdas_betaB
+
+
+def magn_betaB(ising, lambdas_betaB):
+    return np.gradient(np.log(lambdas_betaB), Bs, axis=0) / betas / ising.n
 
 
 if (__name__ == "__main__"):
+
     #   Oppgave 4, a)
     sigma_arr = generate_spin_conf(5)
     print(sigma_arr)
@@ -108,47 +129,65 @@ if (__name__ == "__main__"):
 
     #   Oppgave 4, b)
     is1 = Ising_2D(2)
-    is1.generate_P_matrix()
     print(is1.P_matrix)
 
     #   Oppgave 4, c)
 
     #   Setup
-    n_max = 9
-    beta_low = 0.005
-    beta_high = 1.000
-    steps = 100
-    dbeta = (beta_high - beta_low)/steps
+    n_max = 10
+    betas = np.linspace(0.005, 1.000, 101)
 
-    is_arr = []
-    for i in range(n_max):
-        is_arr.append(Ising_2D(i+1))
-
-    betas = np.linspace(beta_low, beta_high, steps, endpoint=False)
-
-    for ising in is_arr:
+    for i in range(1, n_max + 1):
+        ising = Ising_2D(i)
         print("n=", ising.n)
         cmap = get_cmap(ising.P_len)
-        lambdas_arr = lambda_afo_beta(ising, beta_low, beta_high, dbeta)
-        print(lambdas_arr[0])
+        lambdas_arr = lambda_beta(ising, betas)
         plt.figure()
-        plt.title(r"$n$ = " + str(ising.n) + "; $J_{\parallel}$,$J_{\\bot}$ = " + str(ising.J[0]) + "," + str(ising.J[1]) + "; $B$ = " + str(ising.B))
+        plt.title(r"$n$ = " + str(ising.n) + "; $J_{\parallel}$,$J_{\\bot}$ = " + str(
+            ising.J[0]) + "," + str(ising.J[1]) + "; $B$ = " + str(ising.B))
         for i in range(2**ising.n):
-            plt.semilogy(betas, lambdas_arr[:,i], color=cmap(i))
+            plt.semilogy(betas, lambdas_arr[:, i], color=cmap(i))
         plt.xlabel(r"$\beta$", fontsize=16)
         plt.ylabel(r"$\lambda$-values", fontsize=16)
         plt.grid()
-        #plt.savefig(".pdf")
+        plt.savefig(f'eigvals_{ising.n:.1g}.pdf')
         plt.show()
 
     #   Oppgave 4, d)
+    betas = np.linspace(0.005, 1.000, 101)
+    Bs = np.linspace(-1, 1, 201)
+    is1 = Ising_2D(5)
 
+    lambdas = lambda_betaB(is1, Bs, betas)
+    magns = magn_betaB(is1, lambdas)
+    bB, Bb = np.meshgrid(betas, Bs)
 
+    fig, ax = plt.subplots()
+    cs = ax.contourf(bB, Bb, magns, levels=np.linspace(-1, 1, 101))
+    cbar = fig.colorbar(cs, ticks=(-1, 0, 1))
+    cbar.set_label(r'$m/n$')
+    plt.xlabel(r'$\beta$')
+    plt.ylabel(r'$H$')
+    plt.savefig('magnetization.pdf')
 
+    #   Oppgave 4, e)
+    enthalpy = np.gradient(- np.log(lambdas), betas, axis=1) / is1.n
+    spec_heat = - betas**2 * np.gradient(enthalpy, betas, axis=1)
 
+    fig, ax = plt.subplots()
+    cs = ax.contourf(bB, Bb, enthalpy, 100)
+    cbar = fig.colorbar(cs)
+    cbar.set_label(r'$H_e(\beta)/N$')
+    plt.xlabel(r'$\beta$')
+    plt.ylabel(r'$B$')
+    plt.savefig('enthalpy.pdf')
 
+    fig, ax = plt.subplots()
+    cs = ax.contourf(bB, Bb, spec_heat, 100)
+    cbar = fig.colorbar(cs)
+    cbar.set_label(r'$C_B(\beta)/Nnk_B$')
+    plt.xlabel(r'$\beta$')
+    plt.ylabel(r'$B$')
+    plt.savefig('specific_heat.pdf')
 
-
-
-
-
+    plt.show()
